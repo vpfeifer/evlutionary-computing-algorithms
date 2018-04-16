55877a524c2f1295bf3351606301ab00c7c9df3d
Vinicius Pfeifer <Vinicius Pfeifer>
2018-04-15 22:11:35 -0300

    bug fixes and multiple executions

--- trabalho01/ex01/ex01.py
+++ trabalho01/ex01/ex01.py
@@ -3,18 +3,28 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plotter
 
-population_size = 8
+# 0 - Imprime somente a geracao resultante
+# 1 - Imprime o resultado a cada geracao (populacao, aptidao de cada individuo, aptidao media)
+# 2 - Imprime os passos do algoritmo (selecao, reproducao, mutacao, avaliacao)
+# 3 - Imprime os detalhes da selecao dos individuos da proxima geracao
+verbose_level = 0
+
+population_size = 32
 
 target = [1,1,1,1,0,1,1,0,1,1,1,1]
 targetSize = len(target)
 
-crossover_rate = 0
-mutation_rate = 0.5
+crossover_rate = 0.5
+mutation_rate = 0.02
+
+execution_times = 1000
 
 print "Reconhecer o 0 representado por",target
+print "Verbose level", verbose_level
 print "Tamanho da populacao :",population_size
 print "Taxa de crossover :", crossover_rate
 print "Taxa de mutacao :", mutation_rate
+print "Numero de execucoes",execution_times
 
 # Retorna um bit aleatorio que representa um gene.
 def gene():
@@ -29,11 +39,11 @@ def random_population_init():
         counter += 1
     return initial_population
 
-def print_population(population):
-    print "\nPopulacao\n"
+def print_list(list, list_name):
+    print "\n", list_name, "\n"
     i = 1
-    for individual in population:
-        print i, "-", individual
+    for item in list:
+        print i, "-   ", item
         i += 1
 
 # Conta a quantidade de genes diferentes de um individuo em relacao ao alvo.
@@ -60,29 +70,61 @@ def evaluate(population):
     aptitudes = []
     for item in h:
         aptitudes.append(targetSize - item)
+    if verbose_level > 1:        
+        print "\n=== EVALUATION STEP ==="
+        print "Target", target
+        print_list(population, "Population")
+        print "Target size", targetSize
+        print "Hamming Array",h
+        print "Aptitudes result", aptitudes
     return aptitudes
 
 # Gera o vetor com as faixas em graus da roleta
-def get_selection_range(aptitudes):
-    generationAptitude = sum(aptitudes)
+def get_selection_ranges(aptitudes):
     init_range = 0
-    selection_range = []
+    selection_ranges = []
+    generationAptitude = sum(aptitudes)
     for aptitude in aptitudes:
-        individualRangeSize = float (360 * aptitude)/generationAptitude
-        selection_range.append([init_range, individualRangeSize + init_range])
+        individualRangeSize = 0
+        if generationAptitude == 0:
+            individualRangeSize = 360/population_size
+        else:
+            individualRangeSize = float (360 * aptitude)/generationAptitude
+        selection_ranges.append([init_range, individualRangeSize + init_range])
         init_range += individualRangeSize
-    return selection_range
+    if verbose_level > 2:
+        print "\n\t=== GET SELECTION RANGES ==="
+        print "\tAptitudes =", aptitudes
+        print "\tSum aptitudes =",generationAptitude
+        print_list(selection_ranges, "Selection Ranges")
+    return selection_ranges
 
 # Realiza a selecao dos individuos para proxima geracao
 def selection(aptitudes, population):
-    selection_ranges = get_selection_range(aptitudes)
+    if verbose_level > 1:
+        print "\n=== SELECTION STEP ==="
+    selection_ranges = get_selection_ranges(aptitudes)
     selection_randoms = np.random.randint(360, size=population_size)
+    if verbose_level > 1:
+        print "Target", target
+        print_list(population, "Population")
+        print "Aptitudes", aptitudes
+        print_list(selection_ranges,"Selection ranges")
+        print "Random numbers", selection_randoms
     newGeneration = []
     for srand in selection_randoms:
+        if verbose_level > 2:
+            print "\n\tThe random number", srand
+        i = 0
         for srange in selection_ranges:
             if srand >= srange[0] and srand <= srange[1]:
-                index = selection_ranges.index(srange)
-                newGeneration.append(population[index])
+                if verbose_level > 2:
+                    print "\tMatch with", srange
+                    print "\tThen the individual",i, " - ",population[i],"from the population was selected"
+                newGeneration.append(population[i])
+            i+=1
+    if verbose_level > 1:
+        print_list(newGeneration, "New Generation Selected")
     return newGeneration
 
 # Faz o crossover e atualiza a populacao substituindo
@@ -109,25 +151,46 @@ def genetic_recombination(parent1, parent2, crossover_point):
 def reproduction(population):
     r = [random.random(), random.random(), random.random(), random.random()]
     for item in r:
+        i = 0
         if item < crossover_rate:
-            couple_index = r.index(item)
-            make_crossover(population, couple_index)
+            make_crossover(population, i)
+        i+=1
+    if verbose_level > 1:
+        print "\n=== REPRODUCTION STEP ==="
+        print "Target", target
+        print_list(population, "Population")
+        print "Crossover rate =", crossover_rate
+        print "Random numbers =",r
+        print_list(population, "Population after reproduction")
 
 # Realiza as mutacoes na populacao baseado na taxa de mutacao
 def mutation(population):
+    if verbose_level > 1:
+        print "\n=== MUTATION STEP ==="
+        print "Mutation rate =", mutation_rate
+        print "Target", target
+        print_list(population, "Population")
+    mutation_points = []
+    population_index = 0
     for individual in population:
-        i = 0
-        while i < targetSize:
+        individual_index = 0
+        while individual_index < targetSize:
             if random.random() < mutation_rate:
-                individual[i] = -individual[i] + 1
-            i+=1
+                mutation_points.append([population_index+1, individual_index+1])
+                individual[individual_index] = -individual[individual_index] + 1
+            individual_index+=1
+        population_index+=1
+    if verbose_level > 1:
+        print "Tota mutations",len(mutation_points)
+        print "Mutation points [Individual, Gene] =", mutation_points
+        print_list(population, "Population after mutation")
 
 # Imprime os dados de uma geracao  
 def print_generation_data(generationNumber, population, aptitudes):
-    print "======== Geracao", generationNumber, " ========"
-    #print print_population(population)
+    print "\n======== Geracao", generationNumber, " ========"
+    print print_list(population, "Population")
     print "\nAptidoes =",aptitudes
-    print "\nAvaliacao media da geracao =", float(sum(aptitudes)/population_size)
+    print "\nAptidao media da geracao =", float(sum(aptitudes)/population_size)
 
 # Condicao de parada
 def population_contains_target(aptitudes):
@@ -136,31 +199,89 @@ def population_contains_target(aptitudes):
             return True
     return False
 
-population = random_population_init()
-aptitudes = evaluate(population)
-
-generations = []
-aptitude_averages = []
-
-generationsCounter = 1
-while not population_contains_target(aptitudes):
-    population = selection(aptitudes, population)
-    reproduction(population)
-    mutation(population)
+def execute_algorithm(generate_execution_chart):
+    population = random_population_init()
     aptitudes = evaluate(population)
-    generations.append(generationsCounter)
-    aptitude_avg = float(sum(aptitudes)/population_size)
-    aptitude_averages.append(aptitude_avg)
-    print generationsCounter
-    generationsCounter += 1
 
-print_generation_data(generationsCounter, population, aptitudes)
+    # Dados para o grafico de uma execucao do algoritmo
+    generations = []
+    aptitude_averages = []
+    max_aptitudes = []
+    min_aptitudes = []
+
+    generationsCounter = 1
+    while not population_contains_target(aptitudes):
+        # Passos do algoritmo
+        population = selection(aptitudes, population)
+        reproduction(population)
+        mutation(population)
+        aptitudes = evaluate(population)
+
+        # Dados para o grafico
+        generations.append(generationsCounter)
+        aptitude_avg = float(sum(aptitudes)/population_size)
+        aptitude_averages.append(aptitude_avg)
+        max_aptitudes.append(max(aptitudes))
+        min_aptitudes.append(min(aptitudes))
+        
+        if verbose_level > 0:
+            print_generation_data(generationsCounter, population, aptitudes)
+        generationsCounter += 1
+
+    if generate_execution_chart:
+        print_generation_data(generationsCounter, population, aptitudes)
+
+        fig, ax = plotter.subplots()
+        ax.plot(generations, aptitude_averages, label='Population Aptitude Average')
+        ax.plot(generations, max_aptitudes, label='Max Aptitude')
+        ax.plot(generations, min_aptitudes, label='Min Aptitude')
+
+        handles, labels = ax.get_legend_handles_labels()
+        ax.legend(handles, labels)
+
+        ax.set(xlabel='Generations', ylabel='Aptitude',title='')
+        ax.grid()
+
+        fig.savefig("test.png")
+        plotter.show()
+    return generationsCounter
+
+executed = 0
+
+# Dados para o grafico de todas as execucoes
+generations_needed_array = []
+avg_array = []
+max_array = []
+min_array = []
+executions = []
+
+while executed < execution_times:
+    generations_needed = execute_algorithm(False)
+    generations_needed_array.append(generations_needed)
+    executions.append(executed + 1)
+    executed += 1
+
+a = 0
+avg_generations = sum(generations_needed_array)/len(generations_needed_array)
+max_val = max(generations_needed_array)
+min_val = min(generations_needed_array)
+
+while a < execution_times:
+    max_array.append(max_val)
+    min_array.append(min_val)
+    avg_array.append(avg_generations)
+    a+=1
 
 fig, ax = plotter.subplots()
-ax.plot(generations,aptitude_averages)
+ax.plot(executions, generations_needed_array, label='Generations Needed')
+ax.plot(executions, avg_array, label='Generations average')
+ax.plot(executions, min_array, label='Min Generation')
+ax.plot(executions, max_array, label='Max Generation')
 
-ax.set(xlabel='Generations', ylabel='Aptitude',
-       title='')
+handles, labels = ax.get_legend_handles_labels()
+ax.legend(handles, labels)
+
+ax.set(xlabel='Executions')
 ax.grid()
 
 fig.savefig("test.png")
