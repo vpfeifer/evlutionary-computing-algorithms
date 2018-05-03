313677c556f462f5729b216762e6da130c92faf5
Vinicius Pfeifer <vpfeifer20@gmail.com>
2018-05-03 17:27:42 -0300

    start perceptron training function

--- perceptron/perceptron.py
+++ perceptron/perceptron.py
@@ -1,10 +1,17 @@
 import random
 
+# Perceptron to recognize types of iris flowers
+
+training_flowers_factor = 0.8       # Percentage of dataset to be used for training
+validate_flowers_factor = 0.1       # Percentage of dataset to be used for validation
+test_flowers_factor = 0.1           # Percentage of dataset to be used for test
+
+# Class to represent the flower
 class IrisFlower(object):
-    sepal_length = 0
-    sepal_width = 0
-    petal_length = 0
-    petal_width = 0
+    sepal_length = 0.0
+    sepal_width = 0.0
+    petal_length = 0.0
+    petal_width = 0.0
     name = "none"
 
     def __init__(self, sepal_length, sepal_width, petal_length, petal_width, name):
@@ -14,36 +21,76 @@ class IrisFlower(object):
         self.petal_width = petal_width
         self.name = name
 
+# Print a flower on console
 def print_flower(flower):
-    print "Iris Flower"
     print "Sepal length:", flower.sepal_length
     print "Sepal width:", flower.sepal_width
     print "Petal length:", flower.petal_length
     print "Petal width:", flower.petal_width
     print "Type:", flower.name
 
-def get_flowers_subset(all_flowers, factor):
+# Reads the dataset file
+def read_dataset_file():
+    iris_data_file = open('iris.data', 'r')
+    
+    all_flowers = []
+    for line in iris_data_file: 
+        data = line.split(',')
+        iris_flower = IrisFlower(float(data[0]), float(data[1]), float(data[2]), float(data[3]), data[4])
+        all_flowers.append(iris_flower)
+    return all_flowers
+
+# Gets the given amount of dataset elements randomly
+def get_flowers_subset(all_flowers, amount):
     subset = []
-    all_flowers_len = len(all_flowers)
     counter = 0
-    while counter < all_flowers_len * factor:
-        index = random.randint(0, all_flowers_len-1)
-        subset.append(all_flowers[index])
+    while counter < amount:
+        index = random.randint(0, len(all_flowers)-1)
+        flower = all_flowers[index]
+        subset.append(flower)
+        all_flowers.remove(flower)
         counter += 1
     return subset
 
-iris_data_file = open('iris.data', 'r')
+flowers = read_dataset_file()
+flowers_len = len(flowers)
 
-all_flowers = []
-for line in iris_data_file: 
-    data = line.split(',')
-    iris_flower = IrisFlower(data[0], data[1], data[2], data[3], data[4])
-    all_flowers.append(iris_flower)
+training_set_size = flowers_len * training_flowers_factor
+training_set = get_flowers_subset(flowers, training_set_size)
 
-training_flowers_factor = 0.8
-validate_flowers_factor = 0.1
-test_flowers_factor = 0.1
+validate_set_size = flowers_len * validate_flowers_factor
+validate_set = get_flowers_subset(flowers, validate_set_size)
 
-training_set = get_flowers_subset(all_flowers, training_flowers_factor)
+test_set_size = flowers_len * test_flowers_factor
+test_set = get_flowers_subset(flowers, test_set_size)
 
-print len(training_set)
+
+def perceptron(flower, w, b):
+    print "Received flower \n", print_flower(flower)
+    print w
+    print b
+    y = 0
+    for ws in w:
+        sum_in = 0.0
+        sum_in =  flower.sepal_length * ws[0] 
+        sum_in += flower.sepal_width  * ws[1]
+        sum_in += flower.petal_length * ws[2]
+        sum_in += flower.petal_width  * ws[3]
+        sum_in += b
+        print "n", y, "outs", sum_in
+        y+=1
+
+def train_perceptron():
+    w = [[0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0]]    # Weights matrix
+    b = 0.0                                                                   # Bias
+    max_it = 1000                                                             # Max iterations
+    it = 0                                                                    # Current iteration
+    error = 1                                                                 # Calculated error
+
+    while it < max_it and error > 0:
+        error = 0
+        for flower in training_set:
+            perceptron(flower, w, b)
+        it += 1
+
+train_perceptron()
